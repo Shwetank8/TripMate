263731b09bd5217f434b82521de3fe3765f88536
Shwetank8 <sinhashwetank74@gmail.com>
2025-03-09 23:36:51 +0530

    change in view-trip

--- src/pages/ViewTrip.tsx
+++ src/pages/ViewTrip.tsx
@@ -1,43 +1,50 @@
-import Itinerary from "../components/Itinerary";
-import Hotels from "../components/Hotels";
-import InfoSection from "../components/InfoSection";
-import { db } from "../utils/firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+
+
+import Itinerary from "../components/Itinerary"
+import Hotels from "../components/Hotels"
+import InfoSection from "../components/InfoSection"
+import { db } from "../utils/firebaseConfig"
+import { doc, getDoc } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+// Import the useTheme hook
 
 function ViewTrip() {
-    const { id } = useParams<{ id: any }>();
-    const [trip,setTrip] = useState<any>({});
-   
-    useEffect(() => {
-        getTripData();
-    }, [id]);
+  const { id } = useParams<{ id: any }>()
+  const [trip, setTrip] = useState<any>({})
+ // Get the current theme
 
-    const getTripData = async() =>{
-        try{
-            const docRef = doc(db,'Trips',id);
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                console.log(docSnap.data());
-                setTrip(docSnap.data());
-            }
-            else{
-                console.log('No such document');
-            }
-        }
-        catch(e){
-            console.error('Error getting document:', e);  
-        }  
+  useEffect(() => {
+    getTripData()
+  }, [id])
+
+  const getTripData = async () => {
+    try {
+      const docRef = doc(db, "Trips", id)
+      const docSnap = await getDoc(docRef)
+      if (docSnap.exists()) {
+        console.log(docSnap.data())
+        setTrip(docSnap.data())
+      } else {
+        console.log("No such document")
+      }
+    } catch (e) {
+      console.error("Error getting document:", e)
     }
-    
-    
-    return(
-        <div className="p-10 md:px-20 lg:px-44 xl:px-56">
-          <InfoSection tripData={trip}/>
-          <Hotels tripData = {trip}/>
-          <Itinerary tripData = {trip}/>
-        </div>
-    )
+  }
+
+  return (
+    <div >
+      <div className="flex justify-end mb-4">
+        
+      </div>
+      <InfoSection tripData={trip} />
+      <Hotels tripData={trip} />
+      <Itinerary tripData={trip} />
+    </div>
+  )
+  
 }
-export default ViewTrip;
+
+export default ViewTrip
+
